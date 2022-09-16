f4e8161a2f2d9ad09fbfc114fa6965674dbd54e5
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-09-16 17:44:01 +0300

    add InfoEdit page

---
+++ SpotifyClone/src/screens/InfoEdit.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfoEdit = () => {
+  return (
+    <View>
+      <Text>InfoEdit</Text>
+    </View>
+  )
+}
+
+export default InfoEdit
+
+const styles = StyleSheet.create({})
